0c63841e04cfccd060599e5f27033e2d76944029
rcebul01 <roman.cebula@isobar.com>
2020-04-29 18:03:01 +0200

    Initial commit

---
+++ Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Roman Cebula on 28/04/2020.
+//  Copyright © 2020 Roman Cebula. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+  var name: String
+  var image: String
+  
+  init(name: String, image: String) {
+    self.name = name
+    self.image = image
+  }
+}
+
+
